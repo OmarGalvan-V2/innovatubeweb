5a0a380b6e09334f77fc4601000fd0d99cfb70ec
OmarGalvan-V2 <omar_galvan@outlook.es>
2025-11-21 13:33:45 -0600

    fix: se arregla url

--- src/app/enviroments/enviroments.prod.ts
+++ src/app/enviroments/enviroments.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  ENDPOINT_PERSONAL: 'https://ms-innovatube-gateway.onrender.com/Usuarios',
+  ENDPOINT_PERSONAL: 'https://ms-innovatube-gateway-pitq.onrender.com/Usuarios',
   RECAPTCHA_SITE_KEY: '6LfIIQ8sAAAAALnFMRoAtfbOaD2XUE9dKpj3VtOk',
   YOUTUBE_API: 'AIzaSyCHxlRUXgQXNjDoGEP0yDDjGyh9Tr_iLgU',
   ENDPOINT_YOUTUBE: 'https://www.googleapis.com/youtube/v3/'
